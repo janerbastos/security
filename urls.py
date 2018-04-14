2022a4e4379342c988a32dc99e25738bebcd391a
Janer B. Melo <janerbastos@gmail.com>
2018-04-13 23:07:05 -0300

    Remoção de imports não mais utilizados

--- urls.py
+++ urls.py
@@ -2,7 +2,6 @@
 
 from django.conf.urls import url
 
-from manager.views import create_update_site, open_site, create_update_user, list_user
 from security.views import _logout, _login
 
 urlpatterns = [
